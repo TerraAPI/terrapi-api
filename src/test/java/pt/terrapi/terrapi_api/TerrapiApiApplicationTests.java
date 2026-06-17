7a561ab477616fc5754f8f8352aeda772672bdd8
Ratmir Mukazhanov <59826524+ratmir-mukazhanov@users.noreply.github.com>
2026-06-17 16:13:07 +0100

    add database configuration, Docker setup, and test profile

--- src/test/java/pt/terrapi/terrapi_api/TerrapiApiApplicationTests.java
+++ src/test/java/pt/terrapi/terrapi_api/TerrapiApiApplicationTests.java
@@ -2,8 +2,10 @@ package pt.terrapi.terrapi_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TerrapiApiApplicationTests {
 
 	@Test

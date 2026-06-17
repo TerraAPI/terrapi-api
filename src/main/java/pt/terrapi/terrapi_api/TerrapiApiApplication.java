fdece8d8f07800f5b431a6a5e80c32c1ce90e8e0
Ratmir Mukazhanov <59826524+ratmir-mukazhanov@users.noreply.github.com>
2026-06-17 14:36:52 +0100

    initialize Spring Boot project

---
+++ src/main/java/pt/terrapi/terrapi_api/TerrapiApiApplication.java
@@ -0,0 +1,13 @@
+package pt.terrapi.terrapi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrapiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrapiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/terrapi/terrapi_api/TerrapiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.terrapi.terrapi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrapiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
